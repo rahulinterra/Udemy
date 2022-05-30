051a71dc808d60e90858d2ab68767bfd7b841d81
rahulinterra <rahuls@interrait.com>
2022-05-30 16:34:53 +0530

    ngf

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,6 +1,8 @@
-﻿namespace CRUD.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CRUD.Models
 {
-    public class ApplicationUser
+    public class ApplicationUser:IdentityUser
     {
         public string FullName { get; set; }
     }
