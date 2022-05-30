8fb4f71577af51a94ff625bf8ed8f6581f143a90
rahulinterra <rahuls@interrait.com>
2022-05-30 16:27:13 +0530

    fdf

---
+++ WC.cs
@@ -0,0 +1,9 @@
+﻿namespace CRUD
+{
+    public static class WC
+    {
+        public static string Imagepath = @"\Images\product\";
+        public static string SessionCart = "ShoppingCartSession";
+    }
+}
+
